039ecb1677c69ed6dc61ac7e2341409067a522bc
Andres Tebar Moreno <atm64@alu.ua.es>
2020-10-26 21:02:37 +0100

    Primer test - Entidad Equipo

---
+++ src/test/java/madstodolist/EquipoTest.java
@@ -0,0 +1,20 @@
+package madstodolist;
+
+import madstodolist.model.Equipo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EquipoTest {
+
+    @Test
+    public void crearEquipo() {
+        Equipo equipo = new Equipo("Proyecto P1");
+        assertThat(equipo.getNombre()).isEqualTo("Proyecto P1");
+    }
+}
