04491c4ef07025f19a0f550cd73c67be30a3bc3e
Andres Tebar Moreno <atm64@alu.ua.es>
2020-10-26 21:16:23 +0100

    Tercer test - Definición de igualdad entre equipos

--- src/main/java/madstodolist/model/Equipo.java
+++ src/main/java/madstodolist/model/Equipo.java
@@ -27,5 +27,25 @@ public class Equipo implements Serializable{
 
     public Long getId() { return id; }
 
+    public void setId(Long id) { this.id = id; }
+
     public String getNombre() { return nombre; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Equipo equipo = (Equipo) o;
+        if (id != null && equipo.id != null)
+            // Si tenemos los ID, comparamos por ID
+            return Objects.equals(id, equipo.id);
+        // sino comparamos por campos obligatorios
+        return nombre.equals(equipo.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        // Generamos un hash basado en los campos obligatorios
+        return Objects.hash(nombre);
+    }
 }
