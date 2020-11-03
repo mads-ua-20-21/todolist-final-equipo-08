0cfb09095af98b56a3e3f9257afaecee1875a04e
Andres Tebar Moreno <atm64@alu.ua.es>
2020-11-03 20:01:45 +0100

    Implementado crear nuevo equipo

--- src/main/java/madstodolist/controller/EquipoController.java
+++ src/main/java/madstodolist/controller/EquipoController.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -59,5 +62,31 @@ public class EquipoController {
         List<Usuario> usuarios = equipoService.usuariosEquipo(idEquipo);
         model.addAttribute("usuarios", usuarios);
         return "usuariosEquipo";
+    }
+
+    @GetMapping("/equipos/nuevo")
+    public String formNuevoEquipo(@ModelAttribute Equipo equipo, Model model, HttpSession session){
+
+        managerUserSesion.usuarioLogueado(session);
+        Usuario usuario = usuarioService.findById((Long)session.getAttribute("idUsuarioLogeado"));
+        if (usuario == null) {
+            throw new UsuarioNotFoundException();
+        }
+        model.addAttribute("usuario", usuario);
+        return "formNuevoEquipo";
+    }
+
+    @PostMapping("/equipos/nuevo")
+    public String nuevoEquipo(@ModelAttribute Equipo equipo, Model model, RedirectAttributes flash,
+                              HttpSession session){
+
+        managerUserSesion.usuarioLogueado(session);
+        Usuario usuario = usuarioService.findById((Long)session.getAttribute("idUsuarioLogeado"));
+        if (usuario == null) {
+            throw new UsuarioNotFoundException();
+        }
+        equipoService.nuevoEquipo(equipo.getNombre());
+        flash.addFlashAttribute("mensaje", "Equipo creada correctamente");
+        return "redirect:/equipos";
     }
 }
