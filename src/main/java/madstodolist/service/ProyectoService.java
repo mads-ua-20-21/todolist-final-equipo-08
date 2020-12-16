6acfa3bc3f40d49af0eaf494375a09067a7ab3c9
Raúl García Carrión <rgc76@alu.ua.es>
2020-12-16 10:53:29 +0100

    Funcionalidades de la capa de servicio de Proyecto creados

--- src/main/java/madstodolist/service/ProyectoService.java
+++ src/main/java/madstodolist/service/ProyectoService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProyectoService {
 
@@ -28,8 +31,53 @@ public class ProyectoService {
     public Proyecto comprobarIdProyecto(Long idProyecto){
         Proyecto proyecto = proyectoRepository.findById(idProyecto).orElse(null);
         if (proyecto == null){
-            throw new EquipoServiceException("Equipo " + idProyecto + " no existe ");
+            throw new ProyectoServiceException("El proyecto " + idProyecto + " no existe ");
         }
         return proyecto;
     }
+
+    @Transactional(readOnly = true)
+    public List<Tarea> tareasProyecto(Long idProyecto){
+
+        Proyecto proyecto = comprobarIdProyecto(idProyecto);
+        List<Tarea> tareas = new ArrayList(proyecto.getTareas());
+        return tareas;
+    }
+
+    @Transactional
+    public Proyecto nuevoProyecto (Long idEquipo, String nombre){
+        Equipo equipo = equipoRepository.findById(idEquipo).orElse(null);
+        if (equipo == null){
+            throw new ProyectoServiceException("El equipo " + idEquipo + " no existe ");
+        }
+
+        Proyecto proyecto = new Proyecto(nombre,equipo);
+        proyecto = proyectoRepository.save(proyecto);
+        return proyecto;
+    }
+
+    @Transactional
+    public void borrarProyecto (Long idProyecto){
+        Proyecto proyecto = proyectoRepository.findById(idProyecto).orElse(null);
+        if (proyecto == null){
+            throw new ProyectoServiceException("El proyecto " + idProyecto + " no existe ");
+        }
+        proyectoRepository.delete(proyecto);
+    }
+
+    @Transactional
+    public Proyecto modificarProyecto (Long idProyecto, String nombre){
+        Proyecto proyecto = proyectoRepository.findById(idProyecto).orElse(null);
+        if (proyecto == null){
+            throw new ProyectoServiceException("El proyecto " + idProyecto + " no existe ");
+        }
+
+        proyecto.setNombre(nombre);
+        proyecto = proyectoRepository.save(proyecto);
+
+        return proyecto;
+    }
+
+
+
 }
