9348c0d6e58c7cb96a5dcb720d7b4243bd62e262
Raúl García Carrión <rgc76@alu.ua.es>
2020-12-20 11:44:56 +0100

    Creados méotodos Controller para crear y eliminar un comentario

--- src/main/java/madstodolist/controller/ComentarioController.java
+++ src/main/java/madstodolist/controller/ComentarioController.java
@@ -1,10 +1,7 @@
 package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSesion;
-import madstodolist.controller.exception.ProyectoNotContainsTareaException;
-import madstodolist.controller.exception.ProyectoNotFoundException;
-import madstodolist.controller.exception.TareaNotFoundException;
-import madstodolist.controller.exception.UsuarioNotFoundException;
+import madstodolist.controller.exception.*;
 import madstodolist.model.*;
 import madstodolist.service.ComentarioService;
 import madstodolist.service.ProyectoService;
@@ -14,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -65,5 +65,66 @@ public class ComentarioController {
         List<Comentario> comentarios = tareaService.comentariosTarea(idTarea);
         model.addAttribute("comentarios", comentarios);
         return "comentariosTarea";
+    }
+
+    @PostMapping("/usuarios/{id}/proyectos/{proyecto}/tareas/{tarea}")
+    public String nuevoComentario(@PathVariable(value="id") Long idUsuario,
+                                  @PathVariable(value="proyecto") Long idProyecto,
+                                  @PathVariable(value="tarea") Long idTarea,
+                                @ModelAttribute Comentario comentario,
+                                Model model, RedirectAttributes flash,
+                                HttpSession session) {
+
+        managerUserSesion.comprobarUsuarioLogeado(session, idUsuario);
+        Usuario usuario = usuarioService.findById((Long)session.getAttribute("idUsuarioLogeado"));
+        if (usuario == null) {
+            throw new UsuarioNotFoundException();
+        }
+        model.addAttribute("usuario", usuario);
+
+        Proyecto proyecto = proyectoService.findById(idProyecto);
+        if (proyecto == null){
+            throw new ProyectoNotFoundException();
+        }
+        Tarea tarea = tareaService.findById(idTarea);
+        if (tarea == null) {
+            throw new TareaNotFoundException();
+        }
+        if (!proyectoService.tareasProyecto(idProyecto).contains(tarea)){
+            throw  new ProyectoNotContainsTareaException();
+        }
+
+        comentarioService.nuevoComentario(comentario.getMensaje(),usuario,tarea);
+        flash.addFlashAttribute("mensaje", "Comentario creado correctamente");
+        return "redirect:/usuarios/" + idUsuario + "/proyectos/" + idProyecto + "/tareas/" + idTarea;
+    }
+
+    @PostMapping("/comentarios/{id}/eliminar")
+    public String eliminarComentario(@PathVariable(value="id") Long idComentario, Model model,
+                                   RedirectAttributes flash, HttpSession session){
+
+        Comentario comentario = comentarioService.findById(idComentario);
+        if (comentario == null) {
+            throw new ComentarioNotFoundException();
+        }
+
+        managerUserSesion.usuarioLogueado(session);
+
+        Usuario usuario = usuarioService.findById((Long)session.getAttribute("idUsuarioLogeado"));
+        if (usuario == null) {
+            throw new UsuarioNotFoundException();
+        }
+
+        //Comprobamos que el usuario logeado es el creador del comentario
+        if (!comentario.getUsuario().equals(usuario)){
+            managerUserSesion.comprobarUsuarioAdministrador(session,(Boolean) session.getAttribute("administrador"));
+        }
+
+        comentarioService.eliminarComentario(usuario.getId(),idComentario);
+        flash.addFlashAttribute("mensaje", "Comentario eliminado correctamente");
+
+        return "redirect:/usuarios/" + usuario.getId() +
+                "/proyectos/" + comentario.getTarea().getProyecto().getId() +
+                "/tareas/" + comentario.getTarea().getId();
     }
 }
