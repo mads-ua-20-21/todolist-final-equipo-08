a24db4432248a2a76a96a78707948e2a07101f25
Andres Tebar Moreno <atm64@alu.ua.es>
2020-12-21 23:16:37 +0100

    Ordenados comentarios por fecha

--- src/main/java/madstodolist/service/ComentarioService.java
+++ src/main/java/madstodolist/service/ComentarioService.java
@@ -37,8 +37,6 @@ public class ComentarioService {
     public List<Comentario> findAllOrderedByName(){
 
         List<Comentario> comentarios = comentarioRepository.findAll();
-        //comentarios.sort(Comparator.comparing(Comentario::getFechaHoraString));
-        comentarios.sort(Comparator.comparing(Comentario::getFechaHora));
         return comentarios;
     }
 
